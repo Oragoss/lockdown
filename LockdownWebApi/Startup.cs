47b0a0efcce7f20c738c970d2829c1c3190e72b2
unknown <interactiveemerald@gmail.com>
2019-02-09 15:57:19 -0600

    Changed the gitignore

--- LockdownWebApi/Startup.cs
+++ LockdownWebApi/Startup.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace LockdownWebApi
 {
@@ -51,6 +44,8 @@ namespace LockdownWebApi
                 app.UseHsts();
             }
 
+            //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/static-files?view=aspnetcore-2.2
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
